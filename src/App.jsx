5ef920faedfd66b1dbfb98536b020f6d289334bb
Ruiseco <acisneros@expediagroup.com>
2023-03-29 16:42:33 -0600

    Added working experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import data from "./utils/experience.json";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const general = data.general;
+  const experience = data.experience;
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <h1>{general.name}</h1>
+      <h2>Contact</h2>
+      <ul>
+        <li>Email: {general.email}</li>
+        <li>Phone: {general.phone}</li>
+      </ul>
+      <h2>Experience</h2>
+      {experience.map((job) => (
+        <div key={job.company}>
+          <h3>{job.company}</h3>
+          <p>{job.title}</p>
+          <p>
+            {job.startDate} - {job.endDate}
+          </p>
+          <p>{job.description}</p>
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default App
+export default App;
