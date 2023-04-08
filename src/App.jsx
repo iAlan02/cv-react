cba54a16152d2fe52c59fa59ceb0ec9e9ee884b5
Ruiseco <acisneros@expediagroup.com>
2023-04-08 11:34:45 -0600

    Fixed languaje

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,37 @@ function App() {
   const experience = data.experience;
   return (
     <>
-      <h1>{general.name}</h1>
-      <h2>Contact</h2>
-      <ul>
-        <li>Email: {general.email}</li>
-        <li>Phone: {general.phone}</li>
-      </ul>
-      <h2>Experience</h2>
-      {experience.map((job) => (
-        <div key={job.company}>
-          <h3>{job.company}</h3>
-          <p>{job.title}</p>
-          <p>
-            {job.startDate} - {job.endDate}
-          </p>
-          <p>{job.description}</p>
-        </div>
-      ))}
+      <header>
+        <h1>{general.name}</h1>
+        <h2>Contact</h2>
+        <ul>
+          <li>Email: {general.email}</li>
+          <li>Phone: {general.phone}</li>
+        </ul>
+      </header>
+
+      <section>
+        <h2>Professional resume</h2>
+        <p>{general.about}</p>
+      </section>
+
+      <section>
+        <h2>Experience</h2>
+        {experience.map((job) => (
+          <div key={job.company}>
+            <ul>
+              <li>
+                <h3>{job.company}</h3>
+                <p>{job.title}</p>
+                <p>
+                  {job.startDate} - {job.endDate}
+                </p>
+                <p>{job.description}</p>
+              </li>
+            </ul>
+          </div>
+        ))}
+      </section>
     </>
   );
 }
