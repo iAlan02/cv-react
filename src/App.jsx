3f196f9e25da1ac3531911c6ad2a62942ff4eae2
Ruiseco <acisneros@expediagroup.com>
2023-04-18 17:20:27 -0600

    Updated styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,46 @@
 import "./App.css";
-import data from "./utils/experience.json";
+import experienceData from "./utils/experience.json";
 
 function App() {
-  const general = data.general;
-  const experience = data.experience;
+  const general = experienceData.general;
+  const experience = experienceData.experience;
   return (
     <>
-      <header>
-        <h1>{general.name}</h1>
-        <h2>Contact</h2>
+      <section>
+        <header>
+          <h1>{general.name}</h1>
+          <h2>Contact</h2>
+        </header>
         <ul>
           <li>Email: {general.email}</li>
           <li>Phone: {general.phone}</li>
         </ul>
-      </header>
+      </section>
 
       <section>
         <h2>Professional resume</h2>
         <p>{general.about}</p>
       </section>
 
-      <section>
-        <h2>Experience</h2>
-        {experience.map((job) => (
-          <div key={job.company}>
-            <ul>
-              <li>
-                <h3>{job.company}</h3>
-                <p>{job.title}</p>
-                <p>
-                  {job.startDate} - {job.endDate}
-                </p>
-                <p>{job.description}</p>
-              </li>
-            </ul>
-          </div>
-        ))}
-      </section>
+      <main>
+        <section>
+          <h2>Experience</h2>
+          {experience.map((job) => (
+            <article key={job.title}>
+              <h3>{job.title}</h3>
+              <h4>{job.company}</h4>
+              <p>
+                {job.startDate} - {job.endDate}
+              </p>
+              <ul>
+                {job.description.map((desc) => (
+                  <p key={desc}>{desc}</p>
+                ))}
+              </ul>
+            </article>
+          ))}
+        </section>
+      </main>
     </>
   );
 }
